085086f282cb13613184ba9b2c5bbbaca1b05359
Ramwis <alexander.krobot@gmail.com>
2025-03-13 19:01:16 +0100

    change2

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="sk">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Moja prvá stránka v PHP</title>
-    </head>
-    <body>
-        <h1>Vitajte na mojej prvej stránke v PHP!</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja prvá stránka v PHP</title>
+</head>
+<body>
+<h1>Vitajte na mojej prvej stránke v PHP!</h1>
 
 <?php
-    echo "<h1>Henlo world</h1>";
-    echo "Dnes je " . date("d.m.Y") . "<br>";
-    echo "Aktuálny čas je " . date("H:i:s") . "<br>";
+// Definícia funkcie
+function zobrazInfo() {
+    echo "<h2>Henlo world</h2>";
+    echo "Dnes je <strong>" . date("d.m.Y") . "</strong><br>";
+    echo "Aktuálny čas je <strong>" . date("H:i:s") . "</strong><br>";
+}
+
+// Zavolanie funkcie
+zobrazInfo();
 ?>
-    </body>
+
+</body>
 </html>
