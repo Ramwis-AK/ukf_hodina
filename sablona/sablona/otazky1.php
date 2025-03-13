7da4f125dbd214269b861097c0d6a14353dee63b
Ramwis <alexander.krobot@gmail.com>
2025-03-13 19:47:08 +0100

    moje_otazky

---
+++ sablona/sablona/otazky1.php
@@ -0,0 +1,15 @@
+<?php
+// Pole otázok
+$otazky1 = array(
+    "Prečo sa učíte programovanie?",
+    "Ktorý editor kódu používate najradšej?",
+    "Čo vás najviac baví na tvorbe webov?"
+);
+
+// Pole odpovedí
+$odpovede1 = array(
+    "Programovanie mi dáva možnosť tvoriť a riešiť zaujímavé problémy.",
+    "Najradšej používam VS Code, pretože je rýchly a má veľa rozšírení.",
+    "Najviac ma baví vidieť výsledok mojej práce priamo v prehliadači."
+);
+?>
