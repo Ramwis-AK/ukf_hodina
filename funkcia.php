0b679a1bed225fcc0d8b8696ed0a64435683da7b
Ramwis <alexander.krobot@gmail.com>
2025-03-13 19:03:21 +0100

    sep_fun

---
+++ funkcia.php
@@ -0,0 +1,6 @@
+<?php
+function zobrazInfo() {
+    echo "<h2>Henlo world</h2>";
+    echo "Dnes je <strong>" . date("d.m.Y") . "</strong><br>";
+    echo "Aktuálny čas je <strong>" . date("H:i:s") . "</strong><br>";
+}
--- index.php
+++ index.php
@@ -9,14 +9,6 @@
 <h1>Vitajte na mojej prvej stránke v PHP!</h1>
 
 <?php
-// Definícia funkcie
-function zobrazInfo() {
-    echo "<h2>Henlo world</h2>";
-    echo "Dnes je <strong>" . date("d.m.Y") . "</strong><br>";
-    echo "Aktuálny čas je <strong>" . date("H:i:s") . "</strong><br>";
-}
-
-// Zavolanie funkcie
 zobrazInfo();
 ?>
 
